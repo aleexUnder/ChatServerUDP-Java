6a3c02644de3348e6fddcf7f789c74a4cb8c703b
aleexUnder <alsam40@gmail.com>
2018-10-07 13:07:13 +0300

    Revert "На клиенте сделано отображение отправляемых сообщений. Необходимо доделать получаемые. Также нужно наладить отправку/получение сообщений."
    This reverts commit 60641eb6392e563d16279fc4cfb65a7b99484891.

--- ServerLab1/src/Const.java
+++
@@ -1,9 +0,0 @@
-
-public class Const {//класс для связи с конкретной таблицей и ее конкретными полями
-    public static final String USER_TABLE="users";
-    public static final String USER_ID="idUsers";
-    public static final String USER_NICKNAME="nickname";
-    public static final String USER_NAME="UsersName";
-    public static final String USER_SECOND_NAME="UsersSecondName";
-    public static final String USER_PASSWORD="userspassword";
-}
